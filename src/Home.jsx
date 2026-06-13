0ba2e13ee1b9755bf36202c1ea16f4312c9fb300
Gisa <gisaaimesavio@gmail.com>
2026-06-13 21:18:08 +0200

    Adding some CSS on Navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return ( 
-    <div className="flex justify-center items-center ">
-      <h2 className="text-2xl font-semibold mt-5">Homepage</h2>
+    <div className="ml-78 mt-20">
+      <h2 className="text-2xl text-gray-900 font-semibold mt-5">Homepage</h2>
     </div>
    );
 }
--- src/Navabar.jsx
+++ src/Navabar.jsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (  
-    <nav className="flex justify-center items-center mt-15 flex-col ">
-      <h1 className="text-3xl font-bold mb-7">The Ninja Dodjo</h1>
-      <div className="">
-        <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+    <nav className="flex justify-evenly mt-15 items-center">
+      <h1 className="text-3xl font-bold text-red-400">The Dojo Blog</h1>
+      <div>
+        <a className="mr-7 text-gray-600" href="/">Home</a>
+        <a className="text-white bg-red-400 rounded-lg p-2" href="/create">New Blog</a>
       </div>
     </nav>
   );
